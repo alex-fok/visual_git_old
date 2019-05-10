011fde31e762f0477cc61c0dcec29d8c468170cf
Alexander Fok <foktmalexander@gmail.com>
2019-05-10 15:42:59 -0400

    FIXME: css styling in DragBoard.css is not reflected

--- client/src/routes/Display/DragBoard/DragBoard.js
+++ client/src/routes/Display/DragBoard/DragBoard.js
@@ -2,9 +2,11 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import io from 'socket.io-client';
 import svgElementController from './svgElementController';
-import './DragBoard.css'
+import './DragBoard.css';
 
 const svgNS = "http://www.w3.org/2000/svg";
+const containerID = "svgContainer";
+
 class DragBoard extends Component {
 	constructor(props) {
 		super(props);
@@ -19,16 +21,14 @@ class DragBoard extends Component {
 			isDragging: false,
 			updated: false
 		}
-
-		//this.handleNewSVGElementRequest = this.handleNewSVGElementRequest.bind(this);
 	}
 
 	componentDidMount() {
-		svgElementController.initSocket(this, this.state.socket, {
-			createRectSVGElement: ((data, callback, obj) => { return svgElementController.createRectSVGElement(data, callback, obj)}),
-			appendSVG: ((element) => {svgElementController.appendSVG(element)}),
-			handleMouseDown: ((e, str, obj) => {svgElementController.handleMouseDown(e, str, obj)}),
-			handleMouseOver: ((e, str, obj) => {svgElementController.handleMouseOver(e, str, obj)})
+		svgElementController.initSocket(this, this.state.socket, containerID, {
+			createRectSVGElement: (data => { return svgElementController.createRectSVGElement(...data)}),
+			appendSVG: (data => {svgElementController.appendSVG(...data)}),
+			handleMouseDown: (data => {svgElementController.handleMouseDown(...data)}),
+			handleMouseOver: (data => {svgElementController.handleMouseOver(...data)})
 		});
 	}
 
@@ -44,19 +44,19 @@ class DragBoard extends Component {
 
 		return(
 			<div>
-				<div id="descriptionBox"></div>
+				<div id="messageBox">test</div>
 				<div style={{width:divW, height:divH}}>
 					
 					<div style={{width:svgW*.75, height:svgH*.75}}>
 						<svg
-							id="svgContainer"
+							id={containerID}
 							xmlns={svgNS}
 							viewBox={`0 0 ${svgW*.75} ${svgH*.75}`}
 							width="100%"
 							height="100%"
-							onMouseMove={(e)=>svgElementController.handleMouseMove(e, this)}
-							onMouseLeave={(e)=>svgElementController.notDragged(e, this)}
-							onMouseUp={(e)=>svgElementController.notDragged(e, this)}
+							onMouseMove={(e)=>svgElementController.handleMouseMove(e, this, containerID)}
+							onMouseLeave={(e)=>svgElementController.notDragged(e, this, containerID)}
+							onMouseUp={(e)=>svgElementController.notDragged(e, this, containerID)}
 							style={{backgroundColor: "#999"}}
 						>
 						</svg>
--- client/src/routes/Display/DragBoard/svgElementController.js
+++ client/src/routes/Display/DragBoard/svgElementController.js
@@ -1,7 +1,7 @@
 const svgNS = "http://www.w3.org/2000/svg";
 
 export default {
-	initSocket: (obj, socket, fnList) => {
+	initSocket: (obj, socket, containerID, fnList) => {
 
 		socket.on("svgAdd", (data) => {
 			let {svgElements} = obj.state;
@@ -10,22 +10,22 @@ export default {
 			obj.setState(prev => ({
 				svgElements: Object.assign(svgElements, data)
 			}));
-			fnList.appendSVG(fnList.createRectSVGElement(svgObj, {
+			fnList.appendSVG([fnList.createRectSVGElement([svgObj, containerID, {
 				handleMouseDown: fnList.handleMouseDown,
 				handleMouseOver: fnList.handleMouseOver
-			}, obj));
+			}, obj]), containerID]);
 		});
 
 		socket.on("svgMove", (data) => {
 			const {id, x, y, fill} = data;
-			const rect = document.getElementById("svgContainer").getElementById(id);
+			const rect = document.getElementById(containerID).getElementById(id);
 			rect.setAttributeNS(null, "x", x);
 			rect.setAttributeNS(null, "y", y);
 			rect.setAttributeNS(null, "fill", fill);
 		});
 
 		socket.on("svgReleased", (id) => {
-			const rect = document.getElementById("svgContainer").getElementById(id);
+			const rect = document.getElementById(containerID).getElementById(id);
 			rect.setAttributeNS(null, "fill", "#FFF");
 			obj.setState(prev => ({
 				svgElements: Object.assign(prev.svgElements, {[id]: Object.assign(prev.svgElements[id], {x: rect.x, y: rect.y})})
@@ -51,10 +51,10 @@ export default {
 					updated: true
 				});
 				Object.keys(data).forEach(key => {
-					fnList.appendSVG(fnList.createRectSVGElement(data[key], {
+					fnList.appendSVG([fnList.createRectSVGElement([data[key], containerID, {
 						handleMouseDown: fnList.handleMouseDown,
 						handleMouseOver: fnList.handleMouseOver
-					}, obj));
+					}, obj]), containerID]);
 				})
 			}
 		});
@@ -62,27 +62,33 @@ export default {
 		socket.emit("svgCopyRequest", obj.props.jwt);
 	},
 
-	createRectSVGElement: (data, fnList, obj) => {
+	createRectSVGElement: (attributes, containerID, fnList, obj) => {
 		var rect = document.createElementNS(svgNS,"rect");
-		rect.setAttributeNS(null, "id", data.id);
-		rect.setAttributeNS(null, "x", parseInt(data.x));
-		rect.setAttributeNS(null, "y", parseInt(data.y));
-		rect.setAttributeNS(null, "width", parseInt(data.width));
-		rect.setAttributeNS(null, "height", parseInt(data.height));
-		rect.setAttributeNS(null, "fill", data.fill);
-		rect.addEventListener("mousedown", (e)=>{fnList.handleMouseDown(e, data.id, obj)});
-		rect.addEventListener("mouseover", (e)=>{fnList.handleMouseOver(e, data.id, obj)});
+		rect.setAttributeNS(null, "id", attributes.id);
+		rect.setAttributeNS(null, "x", parseInt(attributes.x));
+		rect.setAttributeNS(null, "y", parseInt(attributes.y));
+		rect.setAttributeNS(null, "width", parseInt(attributes.width));
+		rect.setAttributeNS(null, "height", parseInt(attributes.height));
+		rect.setAttributeNS(null, "fill", attributes.fill);
+		rect.addEventListener("mousedown", (e)=>{fnList.handleMouseDown([e, attributes.id, obj])});
+		rect.addEventListener("mouseover", (e)=>{fnList.handleMouseOver([e, attributes.id, obj, containerID])});
 		return rect;
 	},
 
-	appendSVG(element){
-		document.getElementById("svgContainer").appendChild(element);
+	appendSVG(element, containerID){
+		document.getElementById(containerID).appendChild(element);
 	},
 
-	handleMouseOver(e, str, obj){
+	handleMouseOver(e, str, obj, containerID){
 		if (!obj.state.isDragging) {
 			const svgObj = obj.state.svgElements[str];
 			console.log(`${svgObj.id}: ${svgObj.msg}`);
+			document.getElementById("messageBox").style.display = "block";
+			const s = document.getElementById(containerID).getElementById(str);
+			//document.getElementById("messageBox").style.top = s.getAttributeNS("null", )
+			document.getElementById("messageBox").style.top = s.y - 10;
+			//document.getElementById("messageBox").style.left = `${s.x + 10}+"px"`;
+			console.log(document.getElementById("messageBox").style);
 		}
 	},
 
@@ -102,35 +108,6 @@ export default {
 		}
 	},
 
-	handleMouseMove(e) {
-		if (this.state.isDragging) {
-			const {draggedItem, socket} = this.state;
-			const rect = document.getElementById("svgContainer").getElementById(draggedItem.id);
-
-			const px = parseInt(e.pageX);
-			const py = parseInt(e.pageY);
-			const dx = px - draggedItem.xFrom;
-			const dy = py - draggedItem.yFrom;
-
-			this.setState(prev => ({
-				draggedItem: Object.assign(prev.draggedItem, {xFrom: px, yFrom: py}) 
-			}));
-			
-			const rectX = parseInt(rect.getAttributeNS(null, "x"));
-			const rectY = parseInt(rect.getAttributeNS(null, "y"));
-
-			rect.setAttributeNS(null, "x", (rectX + dx));
-			rect.setAttributeNS(null, "y", (rectY + dy));
-
-			socket.emit("moveSVG", {
-				id: draggedItem.id,
-				x: rectX + dx,
-				y: rectY + dy,
-				fill: "#F00"
-			})
-		}	
-	},
-
 	handleNewSVGElementRequest(e, msg, obj) {
 		const id = "newItem" + Date.now();
 		let data = {
@@ -148,10 +125,10 @@ export default {
 		socket.emit("createSVG", data);
 	},
 
-	handleMouseMove(e, obj) {
+	handleMouseMove(e, obj, containerID) {
 		if (obj.state.isDragging) {
 			const {draggedItem, socket} = obj.state;
-			const rect = document.getElementById("svgContainer").getElementById(draggedItem.id);
+			const rect = document.getElementById(containerID).getElementById(draggedItem.id);
 
 			const px = parseInt(e.pageX);
 			const py = parseInt(e.pageY);
@@ -175,16 +152,16 @@ export default {
 				fill: "#F00"
 			})
 		} else {
-			document.getElementById("descriptionBox").style.top = e.pageX+10;
-			document.getElementById("descriptionBox").style.left = e.pageY-10;
+			document.getElementById("messageBox").style.top = e.pageX+10;
+			document.getElementById("messageBox").style.left = e.pageY-10;
 		}	
 	},
 
-	notDragged(e, obj) {
+	notDragged(e, obj, containerID) {
 
 		if (obj.state.isDragging){
 			const {draggedItem, socket, svgElements} = obj.state;
-			const dragged = document.getElementById("svgContainer").getElementById(draggedItem.id);
+			const dragged = document.getElementById(containerID).getElementById(draggedItem.id);
 			svgElements[draggedItem.id].y = parseInt(dragged.getAttributeNS(null, "y"));
 			svgElements[draggedItem.id].x = parseInt(dragged.getAttributeNS(null, "x"));
 		
