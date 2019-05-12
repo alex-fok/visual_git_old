7ef2a544011ae679688bbefa8b98157c08ecc545
Alexander Fok <foktmalexander@gmail.com>
2019-05-12 00:33:34 -0400

    CSS fixed: Syntax error in webpack.config.js. Line 24: {loader: style-loader}, {loader: style-loader}

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,10 +21,8 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: "style-loader",
-            loader: "css-loader"
-          }
+          {loader: "style-loader"},
+          {loader: "css-loader"}
         ]
       }
     ],
