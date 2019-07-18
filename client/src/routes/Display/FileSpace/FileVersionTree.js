972a2fd5926c5e8a75460fc3f8ab24a0387dd857
Alexander Fok <afok@Virtusa.com>
2019-07-18 16:46:07 -0400

    Edited svg size. Placing placeholder functions

--- client/src/routes/Display/FileSpace/FileVersionTree.js
+++ client/src/routes/Display/FileSpace/FileVersionTree.js
@@ -3,17 +3,31 @@ import React, {Component} from 'react';
 class FileVersionTree extends Component {
 	constructor(props) {
 		super(props);
+		this.showDetails = this.showDetails.bind(this);
+	}
+
+	showDetails() {
+		console.log("showDetails clicked");
 	}
 	render() {
 		const {fileData} = this.props;
 		return (
 			<div style={{width: "1200px", height: "600px"}}>
 			<svg
-				viewBox={`0 0 400 400`}
+				viewBox={`0 0 1200 600`}
 				width="100%"
 				height="100%"
-				style={{backgroundColor: "#CCC"}}
+				style={{backgroundColor: "#F5F5F5"}}
+			>
+			<rect 
+				x="100"
+				y="200"
+				width="40"
+				height="40"
+				fill="#FF8000"
+				onClick={this.showDetails}
 			/>
+			</svg>
 			</div>
 		)
 	}
